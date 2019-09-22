63c3776dea40b3c62e4bbb976a0d34a659cbc3f7
BuildTools <unconfigured@null.spigotmc.org>
2019-09-22 13:54:50 -0400

    Added nuget

--- FirebaseAPI/Properties/AssemblyInfo.cs
+++ FirebaseAPI/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FirebaseAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FireSharpLib")]
+[assembly: AssemblyDescription("A library for Firebase DB written in C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FirebaseAPI")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
